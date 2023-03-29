33160a8d491807eb4abebecef149f29f4eacdfd4
mihinN <anonnimnaka@gmail.com>
2023-03-29 20:19:03 +0530

    Just updated

--- third.lua
+++ third.lua
@@ -5,3 +5,22 @@ table.sort(arr)
 for i = count , #arr do 
         print(arr[i])
 end
+
+-- table second 
+local count_one=1;
+local count_two=1;
+local second_arr = {
+    {23,45,67},
+    {99,999,9999},
+    {69,96,13},
+}
+
+    for i=count_one,#second_arr do
+        print("\n")
+        print(second_arr[i])
+        for x=count_two,#second_arr[i] do
+            print(second_arr[i][x])
+        end
+    end
+
+
