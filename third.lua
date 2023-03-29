906d97b94045fc0c37923b586ccb3fcd28c45b1c
mihinN <anonnimnaka@gmail.com>
2023-03-29 20:05:24 +0530

    Just updated

--- third.lua
+++ third.lua
@@ -1,3 +1,7 @@
--- tables oin lua 
-local arr = {12,23,45}
-print(arr[2])
+-- tables oin lua
+local count=1; 
+local arr = {12,23,45,4,565}
+table.sort(arr)
+for i = count , #arr do 
+        print(arr[i])
+end
