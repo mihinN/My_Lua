ebed51e7736357dfe948990c0c111e8dace6f72e
mihinN <anonnimnaka@gmail.com>
2023-03-30 23:00:33 +0530

    Just updated

--- custom_modules.lua
+++ custom_modules.lua
@@ -1,2 +1,6 @@
 -- custom Modules
 
+local getting_custom = require("mymath")
+local out = getting_custom.add(20,30);
+
+print(out)
---
+++ mymath.lua
@@ -0,0 +1,7 @@
+_G.mymath = {}
+
+function mymath.add(number_1,number_2)
+    return number_1+number_2
+end
+
+return mymath
