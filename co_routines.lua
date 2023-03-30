c738eb3d42a89f565c83a4118124358de5fe22fd
mihinN <anonnimnaka@gmail.com>
2023-03-30 15:36:07 +0530

    Just updated

--- co_routines.lua
+++ co_routines.lua
@@ -1,3 +1,9 @@
 -- co routines in lua 
 -- writing files 
 
+local gettingInput = tostring(io.read());
+print(type(gettingInput))
+
+io.output("readme.txt")
+io.write(gettingInput .. "\n")
+io.close()
