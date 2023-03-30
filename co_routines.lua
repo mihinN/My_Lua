859195f82e4afcd1823e679d5c54b2d13f009584
mihinN <anonnimnaka@gmail.com>
2023-03-30 19:31:17 +0530

    Just updated

--- co_routines.lua
+++ co_routines.lua
@@ -3,7 +3,21 @@
 
 
 io.input("readme.txt")
-local readFile = io.read()
+local readFile = io.read(4)
 io.close()
 
 print(readFile)
+
+-- os 
+
+local start = os.clock()
+
+    for i=1,100 do
+        print(i)
+    end
+
+    local out = os.clock() - start
+
+    io.output("time.txt")
+    io.write(out)
+    io.close()
